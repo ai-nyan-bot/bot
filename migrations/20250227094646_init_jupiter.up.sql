ac3b6b6ead834960fbe8e55f5fc80865b3cd459a
Dev <dev@nyan.bot>
2025-03-06 20:03:19 +0800

    Adds id field to trade tables so that insertion order can be tracked

--- migrations/20250227094646_init_jupiter.up.sql
+++ migrations/20250227094646_init_jupiter.up.sql
@@ -3,62 +3,51 @@
 
 create schema jupiter;
 
-create function jupiter.update_updated_at_column()
-returns trigger as $$
+create function jupiter.update_updated_at_column() returns trigger as $$
 begin
-    NEW.updated_at = timezone('utc', now());
-    return NEW;
+    NEW.updated_at
+= timezone('utc', now());
+return NEW;
 end;
-$$ language plpgsql;
+$$
+language plpgsql;
 
 
 create table jupiter.trade
 (
-    slot                    int8 not null,
-    address_id              int4 not null,
-    token_pair_id           int4 not null,
-    amount_base             numeric(36, 12) not null,
-    amount_quote            numeric(36, 12) not null,
-    price                   numeric(36, 12) not null,
-    is_buy                  boolean not null,
-    timestamp               timestamptz not null,
-    signature               text not null,
+    id            bigserial       not null,
+    slot          int8            not null,
+    address_id    int4            not null,
+    token_pair_id int4            not null,
+    amount_base   numeric(36, 12) not null,
+    amount_quote  numeric(36, 12) not null,
+    price         numeric(36, 12) not null,
+    is_buy        boolean         not null,
+    timestamp     timestamptz     not null,
+    signature     text            not null,
 
-    constraint fk_wallet
-        foreign key (address_id)
-        references solana.address(id),
+    constraint fk_wallet foreign key (address_id) references solana.address (id),
 
-    constraint fk_token_pair
-        foreign key (token_pair_id)
-        references solana.token_pair(id),
+    constraint fk_token_pair foreign key (token_pair_id) references solana.token_pair (id),
 
-    constraint unique_signature
-        unique (token_pair_id, signature)
+    constraint unique_signature unique (token_pair_id, signature)
 
- )  partition by hash (token_pair_id);
- 
-  create index trade_token_pair_idx on jupiter.trade(token_pair_id);
-  
-  create table jupiter.trade_1 partition of jupiter.trade
-      for values with (modulus 8, remainder 0);
-  
-  create table jupiter.trade_2 partition of jupiter.trade
-      for values with (modulus 8, remainder 1);
-  
-  create table jupiter.trade_3 partition of jupiter.trade
-      for values with (modulus 8, remainder 2);
-  
-  create table jupiter.trade_4 partition of jupiter.trade
-      for values with (modulus 8, remainder 3);
-  
-  create table jupiter.trade_5 partition of jupiter.trade
-      for values with (modulus 8, remainder 4);
-  
-  create table jupiter.trade_6 partition of jupiter.trade
-      for values with (modulus 8, remainder 5);
-  
-  create table jupiter.trade_7 partition of jupiter.trade
-      for values with (modulus 8, remainder 6);
- 
-  create table jupiter.trade_8 partition of jupiter.trade
-      for values with (modulus 8, remainder 7);
+) partition by hash (token_pair_id);
+
+create index trade_token_pair_idx on jupiter.trade (token_pair_id);
+
+create table jupiter.trade_1 partition of jupiter.trade for values with (modulus 8, remainder 0);
+
+create table jupiter.trade_2 partition of jupiter.trade for values with (modulus 8, remainder 1);
+
+create table jupiter.trade_3 partition of jupiter.trade for values with (modulus 8, remainder 2);
+
+create table jupiter.trade_4 partition of jupiter.trade for values with (modulus 8, remainder 3);
+
+create table jupiter.trade_5 partition of jupiter.trade for values with (modulus 8, remainder 4);
+
+create table jupiter.trade_6 partition of jupiter.trade for values with (modulus 8, remainder 5);
+
+create table jupiter.trade_7 partition of jupiter.trade for values with (modulus 8, remainder 6);
+
+create table jupiter.trade_8 partition of jupiter.trade for values with (modulus 8, remainder 7);
--- migrations/20250304232748_init_pumpfun.up.sql
+++ migrations/20250304232748_init_pumpfun.up.sql
@@ -3,65 +3,54 @@
 
 create schema pumpfun;
 
-create function pumpfun.update_updated_at_column()
-returns trigger as $$
+create function pumpfun.update_updated_at_column() returns trigger as $$
 begin
-    NEW.updated_at = timezone('utc', now());
-    return NEW;
+    NEW.updated_at
+= timezone('utc', now());
+return NEW;
 end;
-$$ language plpgsql;
+$$
+language plpgsql;
 
 
 create table pumpfun.trade
 (
-    slot                    int8 not null,
-    address_id              int4 not null,
-    token_pair_id           int4 not null,
-    amount_base             numeric(36, 12) not null,
-    amount_quote            numeric(36, 12) not null,
-    price                   numeric(36, 12) not null,
-    is_buy                  boolean not null,
-    timestamp               timestamptz not null,
-    virtual_base_reserves   int8 not null,
-    virtual_quote_reserves  int8 not null,
-    progress                real not null,
-    signature               text not null,
+    id                     bigserial       not null,
+    slot                   int8            not null,
+    address_id             int4            not null,
+    token_pair_id          int4            not null,
+    amount_base            numeric(36, 12) not null,
+    amount_quote           numeric(36, 12) not null,
+    price                  numeric(36, 12) not null,
+    is_buy                 boolean         not null,
+    timestamp              timestamptz     not null,
+    virtual_base_reserves  int8            not null,
+    virtual_quote_reserves int8            not null,
+    progress               real            not null,
+    signature              text            not null,
 
-    constraint fk_wallet
-        foreign key (address_id)
-        references solana.address(id),
+    constraint fk_wallet foreign key (address_id) references solana.address (id),
 
-    constraint fk_token_pair
-        foreign key (token_pair_id)
-        references solana.token_pair(id),
+    constraint fk_token_pair foreign key (token_pair_id) references solana.token_pair (id),
 
-    constraint unique_signature
-        unique (token_pair_id, signature)
+    constraint unique_signature unique (token_pair_id, signature)
 
- )  partition by hash (token_pair_id);
- 
- create index trade_token_pair_idx on pumpfun.trade(token_pair_id);
- 
- create table pumpfun.trade_1 partition of pumpfun.trade
-     for values with (modulus 8, remainder 0);
- 
- create table pumpfun.trade_2 partition of pumpfun.trade
-     for values with (modulus 8, remainder 1);
- 
- create table pumpfun.trade_3 partition of pumpfun.trade
-     for values with (modulus 8, remainder 2);
- 
- create table pumpfun.trade_4 partition of pumpfun.trade
-     for values with (modulus 8, remainder 3);
- 
- create table pumpfun.trade_5 partition of pumpfun.trade
-     for values with (modulus 8, remainder 4);
- 
- create table pumpfun.trade_6 partition of pumpfun.trade
-     for values with (modulus 8, remainder 5);
- 
- create table pumpfun.trade_7 partition of pumpfun.trade
-     for values with (modulus 8, remainder 6);
+) partition by hash (token_pair_id);
 
- create table pumpfun.trade_8 partition of pumpfun.trade
-     for values with (modulus 8, remainder 7);
+create index trade_token_pair_idx on pumpfun.trade (token_pair_id);
+
+create table pumpfun.trade_1 partition of pumpfun.trade for values with (modulus 8, remainder 0);
+
+create table pumpfun.trade_2 partition of pumpfun.trade for values with (modulus 8, remainder 1);
+
+create table pumpfun.trade_3 partition of pumpfun.trade for values with (modulus 8, remainder 2);
+
+create table pumpfun.trade_4 partition of pumpfun.trade for values with (modulus 8, remainder 3);
+
+create table pumpfun.trade_5 partition of pumpfun.trade for values with (modulus 8, remainder 4);
+
+create table pumpfun.trade_6 partition of pumpfun.trade for values with (modulus 8, remainder 5);
+
+create table pumpfun.trade_7 partition of pumpfun.trade for values with (modulus 8, remainder 6);
+
+create table pumpfun.trade_8 partition of pumpfun.trade for values with (modulus 8, remainder 7);
