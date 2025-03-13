cb0967cf5a10af7d534901f29961f7d2b36f1129
Dev <dev@nyan.bot>
2025-03-13 13:55:29 +0800

    Calculate candle progress

--- bin/aggregator/src/pumpfun/candle.rs
+++ bin/aggregator/src/pumpfun/candle.rs
@@ -27,16 +27,19 @@ impl RefreshCandles {
         for partition in Partition::enumerate() {
             let repo_1s = repo.clone();
             let pool_1s = pool.clone();
-            result.push(
-                tokio::spawn(async move {
-                    loop {
-                        let mut tx = pool_1s.begin().await.unwrap();
-                        repo_1s.calculate_1s(&mut tx, partition).await.unwrap();
-                        let _ = tx.commit().await;
-                        tokio::time::sleep(Duration::from_millis(10)).await;
-                    }
-                })
-            );
+            result.push(tokio::spawn(async move {
+                loop {
+                    let mut tx = pool_1s.begin().await.unwrap();
+                    repo_1s.calculate_1s(&mut tx, partition).await.unwrap();
+                    repo_1s
+                        .calculate_progress_1s(&mut tx, partition)
+                        .await
+                        .unwrap();
+                        
+                    let _ = tx.commit().await;
+                    tokio::time::sleep(Duration::from_millis(10)).await;
+                }
+            }));
 
             let repo = repo.clone();
             let pool = pool.clone();
@@ -45,32 +48,44 @@ impl RefreshCandles {
                     let mut tx = pool.begin().await.unwrap();
 
                     repo.calculate_1m(&mut tx, partition).await.unwrap();
-                    repo.calculate_progress_1m(&mut tx, partition).await.unwrap();
+                    repo.calculate_progress_1m(&mut tx, partition)
+                        .await
+                        .unwrap();
                     repo.calculate_mcap_1m(&mut tx, partition).await.unwrap();
                     repo.calculate_usd_1m(&mut tx, partition).await.unwrap();
 
                     repo.calculate_5m(&mut tx, partition).await.unwrap();
-                    repo.calculate_progress_5m(&mut tx, partition).await.unwrap();
+                    repo.calculate_progress_5m(&mut tx, partition)
+                        .await
+                        .unwrap();
                     repo.calculate_mcap_5m(&mut tx, partition).await.unwrap();
                     repo.calculate_usd_5m(&mut tx, partition).await.unwrap();
 
                     repo.calculate_15m(&mut tx, partition).await.unwrap();
-                    repo.calculate_progress_15m(&mut tx, partition).await.unwrap();
+                    repo.calculate_progress_15m(&mut tx, partition)
+                        .await
+                        .unwrap();
                     repo.calculate_mcap_15m(&mut tx, partition).await.unwrap();
                     repo.calculate_usd_15m(&mut tx, partition).await.unwrap();
 
                     repo.calculate_1h(&mut tx, partition).await.unwrap();
-                    repo.calculate_progress_1h(&mut tx, partition).await.unwrap();
+                    repo.calculate_progress_1h(&mut tx, partition)
+                        .await
+                        .unwrap();
                     repo.calculate_mcap_1h(&mut tx, partition).await.unwrap();
                     repo.calculate_usd_1h(&mut tx, partition).await.unwrap();
 
                     repo.calculate_6h(&mut tx, partition).await.unwrap();
-                    repo.calculate_progress_6h(&mut tx, partition).await.unwrap();
+                    repo.calculate_progress_6h(&mut tx, partition)
+                        .await
+                        .unwrap();
                     repo.calculate_mcap_6h(&mut tx, partition).await.unwrap();
                     repo.calculate_usd_6h(&mut tx, partition).await.unwrap();
 
                     repo.calculate_1d(&mut tx, partition).await.unwrap();
-                    repo.calculate_progress_1d(&mut tx, partition).await.unwrap();
+                    repo.calculate_progress_1d(&mut tx, partition)
+                        .await
+                        .unwrap();
                     repo.calculate_mcap_1d(&mut tx, partition).await.unwrap();
                     repo.calculate_usd_1d(&mut tx, partition).await.unwrap();
 
