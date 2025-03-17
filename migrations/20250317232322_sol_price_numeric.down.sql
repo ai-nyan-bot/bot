73b9522e6ffd3c3157156f5836c113642402c7c6
Dev <dev@nyan.bot>
2025-03-18 07:27:31 +0800

    [46] - sol price is numeric

---
+++ migrations/20250317232322_sol_price_numeric.down.sql
@@ -0,0 +1,10 @@
+-- Copyright (c) nyanbot.com 2025.
+-- This file is licensed under the AGPL-3.0-or-later.
+
+alter table solana.sol_price_1m alter column usd type real using usd::real;
+alter table solana.sol_price_5m alter column usd type real using usd::real;
+alter table solana.sol_price_15m alter column usd type real using usd::real;
+alter table solana.sol_price_1h alter column usd type real using usd::real;
+alter table solana.sol_price_6h alter column usd type real using usd::real;
+alter table solana.sol_price_1d alter column usd type real using usd::real;
+
