337b8b47a2d1bf36841158998c0159e9f575b72c
Dev <dev@nyan.bot>
2025-03-19 19:03:13 +0800

    Simplifies connection pool

--- crates/common/src/repo/pool.rs
+++ crates/common/src/repo/pool.rs
@@ -20,24 +20,8 @@ pub async fn setup_pool(postgres_config: &PostgresConfig) -> PgPool {
     let pg_pool_options = PgPoolOptions::new()
         .acquire_slow_threshold(Duration::from_secs(5))
         .min_connections(postgres_config.pool_min.resolve_or(1) as u32)
-        .max_connections(postgres_config.pool_max.resolve_or(8) as u32)
+        .max_connections(postgres_config.pool_max.resolve_or(2) as u32)
         .test_before_acquire(true)
-        .before_acquire(|conn, _meta| {
-            Box::pin(async move {
-                sqlx::query("SET work_mem TO '256MB'")
-                    .execute(&mut *conn)
-                    .await?;
-                Ok(true)
-            })
-        })
-        .after_release(|conn, _meta| {
-            Box::pin(async move {
-                sqlx::query("SET work_mem TO '20MB'")
-                    .execute(&mut *conn)
-                    .await?;
-                Ok(true)
-            })
-        })
         .acquire_timeout(Duration::from_millis(
             postgres_config.timeout_acquire_ms.resolve_or(5_000) as u64,
         ));
